9e45adf1c78360ebd5fb082068a549c5f39b47fd
Suman-bot8927 <sumansamanta993228@gmail.com>
2024-10-09 13:18:49 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,10 @@
+// Basic JavaScript for smooth scroll effect
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
